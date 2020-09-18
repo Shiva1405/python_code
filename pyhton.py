eaac29d7ec181293d512c323612a20a956446a82
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2020-09-18 22:34:21 +0530

    Create pyhton.py

---
+++ pyhton.py
@@ -0,0 +1,10 @@
+list=[]
+n=int(input("enter the size of list1\t"))
+for j in range(0,n):
+    i=int(input("enter the elements\t"))
+    list.append(i)
+print(list)    
+for i in list:
+    if (i>0):
+        print(i)
+    
