d2dc1fe4b741e6ba4e31d0f59cabde0a1e081e2d
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-03-10 10:37:42 +0530

    Update pyhton.py

--- pyhton.py
+++ pyhton.py
@@ -1,10 +1,2 @@
-list=[]
-n=int(input("enter the size of list1\t"))
-for j in range(0,n):
-    i=int(input("enter the elements\t"))
-    list.append(i)
-print(list)    
-for i in list:
-    if (i>0):
-        print(i)
-    
+import keyword #keyword module 
+print(keyword.kwlist)
